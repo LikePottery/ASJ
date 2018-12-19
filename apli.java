03bfd402cd933e2c5ae7192bd8b804f898230920
lyna26 <40266991+lyna26@users.noreply.github.com>
2018-12-19 17:58:24 +0100

    Update apli.java

--- apli.java
+++ apli.java
@@ -2,6 +2,11 @@ package v1;
 
 public class apli {
 	public static void main(String[] args) {
+		Abonne n1 = new Abonne (1);
+		Abonne n2 = new Abonne(2);
+		Serveur s= new Serveur();
+		Socket suck = new Socket("127.0.0.1",2601);
+		System.out.println("Serveur lance sur le port " + s.port);
 		
 		
 	}
